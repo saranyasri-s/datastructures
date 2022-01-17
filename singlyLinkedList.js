260c9caa21de4622859e12fd88ad24d3c7bf8efb
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-01-17 14:13:40 +0530

    added push method to singlyLinkedList

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -10,6 +10,22 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 let list = new SinglyLinkedList();
 console.log(list);
+console.log(list.push(1));
+console.log(list.push(2));
+console.log(list.push(3));
+console.log(list.push(4));
