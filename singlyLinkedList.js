e99a35e3dd2d2e73dcac7273bbd74d58411efd8e
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-01-17 14:32:06 +0530

    added pop method to singlyLinkedList

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -22,6 +22,30 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    let removedNode;
+    if (!this.head) {
+      return undefined;
+    } else if (this.length === 1) {
+      removedNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+    } else {
+      let current = this.head;
+      let prev;
+      while (current.next) {
+        prev = current;
+        current = current.next;
+      }
+
+      prev.next = null;
+      this.tail = prev;
+      removedNode = current;
+      this.length--;
+    }
+    return removedNode;
+  }
 }
 let list = new SinglyLinkedList();
 console.log(list);
@@ -29,3 +53,9 @@ console.log(list.push(1));
 console.log(list.push(2));
 console.log(list.push(3));
 console.log(list.push(4));
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list);
