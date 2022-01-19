6395dd70456b8c939f1e281e49fc858544cf25eb
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-01-19 16:44:54 +0530

    added pop method to doubly linked list

--- DoublyLInkedList.js
+++ DoublyLInkedList.js
@@ -24,9 +24,30 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) {
+      return undefined;
+    } else if (this.length == 1) {
+      let temp = this.tail;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return temp;
+    } else {
+      let temp = this.tail;
+      this.tail = temp.prev;
+      this.tail.next = null;
+      this.length--;
+      return temp;
+    }
+  }
 }
 let list = new DoublyLinkedList();
 console.log(list);
 console.log(list.push(1));
 console.log(list.push(2));
 console.log(list.push(3));
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
