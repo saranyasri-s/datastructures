1da74ec28066d4800cba28064b41dfff0b245257
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-01-20 09:39:23 +0530

    added get method to doubly linked list

--- DoublyLInkedList.js
+++ DoublyLInkedList.js
@@ -75,6 +75,26 @@ class DoublyLinkedList {
       return this;
     }
   }
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    } else {
+      let half = Math.floor((this.length - 1) / 2);
+      if (index <= half) {
+        let current = this.head;
+        for (let i = 1; i <= index; i++) {
+          current = current.next;
+        }
+        return current;
+      } else {
+        let current = this.tail;
+        for (let i = this.length - 2; i >= index; i--) {
+          current = current.prev;
+        }
+        return current;
+      }
+    }
+  }
 }
 let list = new DoublyLinkedList();
 console.log(list);
@@ -95,3 +115,10 @@ console.log(list.unshift(2));
 console.log(list.unshift(3));
 console.log(list.unshift(4));
 console.log(list.unshift(5));
+console.log(list.get(0));
+console.log(list.get(1));
+console.log(list.get(2));
+console.log(list.get(3));
+console.log(list.get(4));
+console.log(list.get(5));
+console.log(list.get(-5));
