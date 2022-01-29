d706385e3d4744a846fb8ad65bb9d1078cb5bef8
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-01-29 12:02:12 +0530

    added removeEdge method to graph

--- Graph.js
+++ Graph.js
@@ -13,6 +13,13 @@ class Graph {
     this.adjacencyList[v2].push(v1);
     return this.adjacencyList;
   }
+  removeEdge(v1, v2) {
+    let indexofV1 = this.adjacencyList[v2].indexOf(v1);
+    let indexofV2 = this.adjacencyList[v1].indexOf(v2);
+    this.adjacencyList[v1].splice(indexofV2, 1);
+    this.adjacencyList[v2].splice(indexofV1, 1);
+    return this.adjacencyList;
+  }
 }
 
 let g = new Graph();
@@ -26,3 +33,5 @@ console.log(g.addEdge("coimbatore", "chennai"));
 console.log(g.addEdge("coimbatore", "madurai"));
 console.log(g.addEdge("coimbatore", "nellai"));
 console.log(g.addEdge("madurai", "chennai"));
+console.log(g.removeEdge("madurai", "chennai"));
+console.log(g.removeEdge("madurai", "coimbatore"));
